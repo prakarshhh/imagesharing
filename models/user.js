e194c27e1f47dbfbefbb7028237e3ade067624b8
Prakarsh kashyap <126262042+prakarshhh@users.noreply.github.com>
2024-07-29 11:44:21 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,11 +1,32 @@
-// models/User.js
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
+// Define schema
 const UserSchema = new Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    imageUrl: { type: String, required: true } //Store image paths
+    imageUrl: { type: String, required: true } // Store image paths
 });
 
+// Hash password before saving user
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Method to compare password
+UserSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+// Create and export model
 module.exports = mongoose.model('User', UserSchema);
